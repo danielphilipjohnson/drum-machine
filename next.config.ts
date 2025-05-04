a7f7d78667983a9e0731df86161694a39b3b3e90
Daniel Philip Johnson <daniel-philip-johnson@outlook.com>
2025-05-04 19:54:58 +0100

    chore: to allow deploy to github pages

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,8 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   output: "export",
+  basePath: "/drum-machine",
+  assetPrefix: "/drum-machine/",
 };
 
 export default nextConfig;
